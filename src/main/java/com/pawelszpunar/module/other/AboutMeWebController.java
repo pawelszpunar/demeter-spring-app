7222d77931a45d41e506df1e34c42b6cedfe67c2
pawelszpunar <78647588+pawelszpunar@users.noreply.github.com>
2021-08-08 11:13:15 +0200

    init commit 3

---
+++ src/main/java/com/pawelszpunar/module/other/AboutMeWebController.java
@@ -0,0 +1,5 @@
+package com.pawelszpunar.module.other;
+
+public class AboutMeWebController {
+    /* todo dodać about me contact i shop */
+}
--- src/main/java/com/pawelszpunar/module/post/PostRestController.java
+++ src/main/java/com/pawelszpunar/module/post/PostRestController.java
@@ -1,16 +1,26 @@
 package com.pawelszpunar.module.post;
 
+import com.pawelszpunar.module.blog.menu.MenuDto;
 import com.pawelszpunar.module.blog.menu.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 public class PostRestController {
 
     @Autowired
-    private MenuService menuService;
-    @Autowired
     private PostService postService;
 
+    // posty w formacie json
+    @GetMapping("/api/blog/post")
+    public List<PostDto> getPost() {
+        return postService.getPosts();
+    }
+
+    /* todo sprawdzic czy jest wszystko */
 
 
-    /* = todo  */
 }
